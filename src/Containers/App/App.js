ec204721cc9e850ca6bb5a8981faf20a093761a6
ishan shahzad <3006761319ishan@gmail.com>
2019-11-05 15:20:55 +0500

    testing component added

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">hello app</div>;
+class App extends React.Component {
+  state = {
+    counter: 0
+  };
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1 className="head">hello app</h1>
+        <h1 className="heading">Hello react</h1>
+        <p>{this.state.counter}</p>
+        <button onClick={this.handleClick}></button>
+      </div>
+    );
+  }
 }
 
 export default App;
