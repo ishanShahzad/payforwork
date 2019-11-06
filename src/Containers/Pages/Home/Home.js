df9120df4933255c67edae740d912c73894da62a
Malik Umar <malikumar3443@gmail.com>
2019-11-06 13:14:22 +0500

    'MalikUmar'

--- src/Containers/Pages/Home/Home.js
+++ src/Containers/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import NavBar from '../../../Components/Home/NavBar/NavBar'
+import TopSkills from '../../../Components/Home/TopSkills/TopSkills'
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <NavBar/>
+                <TopSkills />
             </div>
         )
     }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,30 +1,16 @@
-import React from "react";
+import React from 'react';
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-
-import App from "../Containers/App/App";
+import TopSkills from './Components/TopSkills'
 import { shallow } from "enzyme";
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("APP", () => {
-  it("renders APP component", () => {
-    const wrapper = shallow(<App />);
-    const h1 = wrapper.find("h1.head");
-    const text = h1.text();
-    console.log(text);
-    expect(text).toBe("hello app");
-    const heading = wrapper.find("h1.heading");
-    const headingText = heading.text();
-    console.log(headingText);
-    expect(headingText).toBe("Hello react");
-
-    const button = wrapper.find("button");
-    const counter = wrapper.find("p").text();
-    expect(counter).toEqual("0");
-    button.simulate("click");
-    button.simulate("click");
-    const counter2 = wrapper.find("p").text();
-    expect(counter2).toEqual("2");
-    console.log(counter2);
-  });
+describe("TopSkills", () => {
+it("render without crasdhing",()=>{
+  const wrapper = shallow(<TopSkills />)
+  const li = wrapper.find('li.a')
+  const test = li.text();
+  console.log(test)
+  expect(test).toBe("WordPress Developer")
+})
 });
