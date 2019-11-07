59f3bd04b63c3e36e4158e37fe107a060c92ae01
Store- <jahangirahmedawan007@gmail.com>
2019-11-07 12:33:40 +0500

    Home section one added

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./App.css";
 import Home from "../Pages/Home/Home";
-
 class App extends React.Component {
-  state = {
-    counter: 0
-  };
-  handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  };
+  // state = {
+  //   counter: 0
+  // };
+  // handleClick = () => {
+  //   this.setState({
+  //     counter: this.state.counter + 1
+  //   });
+  // };
   render() {
     return (
       <div className="App">
