21922b5473a29a909ebe20b628706c2ea2b59adc
haseeb <haseebahmad061@gmail.com>
2019-11-07 13:03:33 +0500

    postjob

---
+++ src/Containers/Pages/PostJob/postJob.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import "./postjob.scss";
+import "./postTab.scss";
+import "./postMobile.scss";
+
+
+export default class postJob extends Component {
+    //  onButtonClick = e => {
+    //     e.preventDefault();
+    //     console.log('The link was clicked.');
+    //   }
+    render() {
+        return (
+            <div>
+                 <div className="main">
+                <div className="post-form">
+                    <h1 className="h1" id="heading">Post a Job</h1>
+                    <p className="main-para" id="para">Post your job in minutes. Contact expert freelancers and get your work done.</p>
+                
+                    <ul className="aside">
+                        <li className="list1">
+                            <h3 className="listed" id="head">01 Personal Details</h3>
+                            <div className="img1">
+                                <img className="logo" id="image" src="./images/pj.png"/>
+                                <p className="paragraph" id="paragraph">Add a title for your job.Explain your job in detail.Attach any file if that helps to describe the job.</p>
+                            </div>
+                        </li>
+                        <li className="list2">
+                            <h3 id="list1">02 Job Details</h3>
+                        </li>
+                        <li className="list3">
+                            <h3 id="list2">03 Category and Skills</h3>
+                        </li>
+                        <li className="list4">
+                            <h3 id="list3">04 Budget</h3>
+                        </li>
+                        <li className="list5">
+                            <h3 id="list4">05 Project Special</h3>
+                        </li>
+                    </ul>
+               
+                    <div className="form">
+
+                        <h3 className="label-head" id="main_label">Personal Details</h3>
+
+                        <label className="email" id="label_1">EMAIL ADDRESS</label>
+                        <input className="em" type="email" placeholder="Email Address"/>   
+
+                        <label className="first" id="label_2">FIRST NAME</label>
+                        <input className="name1" type="text" placeholder="First Name"/>   
+
+                        <label className="last" id="label_3">LAST NAME</label>
+                        <input className="name2" type="text" placeholder="Last Name"/>  
+
+                        <label className="password" id="label_4">PASSWORD</label>
+                        <input className="pass" type="password" placeholder="Password"/>  
+
+                        <label className="confirm" id="label_5">CONFIRM PASSWORD</label>
+                        <input className="pass2" type="password" placeholder="Confirm Password"/>  
+
+                        <label className="location" id="label_6">LOCATION</label>
+                        <input className="locate" type="password" placeholder="Karachi, Sindh, Pakistan"/>  
+                        <button className="btn">NEXT ></button>
+                    </div>
+
+
+                </div>
+           </div>
+            </div>
+        )
+    }
+}
---
+++ src/Containers/Pages/PostJob/postJob.test.js
@@ -0,0 +1,113 @@
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import React from "react";
+import Enzyme,{shallow} from "enzyme";
+import PostJob from "./postJob";
+import { wrap } from "module";
+import sinon from "sinon";
+import {mount} from "enzyme";
+
+Enzyme.configure({adapter: new EnzymeAdapter()})
+test('text show', () => {
+    const wrapper = shallow(<PostJob/>)
+    const heading = wrapper.find("#heading")
+    let text = heading.text()
+    expect(text).toBe("Post a Job")
+});
+
+test('para show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const para = wrapper.find("#para")
+    let text = para.text()
+    expect(text).toBe("Post your job in minutes. Contact expert freelancers and get your work done.")
+
+});
+test('head show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const head = wrapper.find("#head")
+    let text = head.text()
+    expect(text).toBe("01 Personal Details")
+});
+
+test('paragraph show', () =>{
+    const wrapper = shallow (<PostJob/>)
+    const paragraph = wrapper.find("#paragraph")
+    let text = paragraph.text()
+    expect(text).toBe("Add a title for your job.Explain your job in detail.Attach any file if that helps to describe the job.")
+});
+
+test('list1 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const list1 = wrapper.find("#list1")
+    let text = list1.text()
+    expect(text).toBe("02 Job Details")
+});
+test('list2 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const list2 = wrapper.find("#list2")
+    let text = list2.text()
+    expect(text).toBe("03 Category and Skills")
+});
+test('list3 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const list3 = wrapper.find("#list3")
+    let text = list3.text()
+    expect(text).toBe("04 Budget")
+});
+test('list4 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const list4 = wrapper.find("#list4")
+    let text = list4.text()
+    expect(text).toBe("05 Project Special")
+});
+
+test('main_label show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const main_label = wrapper.find("#main_label")
+    let text = main_label.text()
+    expect(text).toBe("Personal Details")
+});
+test('label_1 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_1 = wrapper.find("#label_1")
+    let text = label_1.text()
+    expect(text).toBe("EMAIL ADDRESS")
+});
+test('label_2 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_2 = wrapper.find("#label_2")
+    let text = label_2.text()
+    expect(text).toBe("FIRST NAME")
+});
+test('label_3 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_3 = wrapper.find("#label_3")
+    let text = label_3.text()
+    expect(text).toBe("LAST NAME")
+});
+test('label_4 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_4 = wrapper.find("#label_4")
+    let text = label_4.text()
+    expect(text).toBe("PASSWORD")
+});
+test('label_5 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_5 = wrapper.find("#label_5")
+    let text = label_5.text()
+    expect(text).toBe("CONFIRM PASSWORD")
+});
+test('label_6 show', () => {
+    const wrapper = shallow (<PostJob/>)
+    const label_6 = wrapper.find("#label_6")
+    let text = label_6.text()
+    expect(text).toBe("LOCATION")
+});
+
+// it('simulates click events', () => {
+//     const onButtonClick = sinon.spy();
+//     const wrapper = mount((
+//       <PostJob onButtonClick={this.onButtonClick} />
+//     ));
+//     wrapper.find('button').simulate('click');
+//     expect(onButtonClick).to.have.property('callCount', 1);
+//   });
