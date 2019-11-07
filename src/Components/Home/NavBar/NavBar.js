62015b4b63cee6d0778782c40d961684ddd9e897
ishan shahzad <3006761319ishan@gmail.com>
2019-11-08 03:46:21 +0500

    fixed some eslint

--- src/Components/Home/NavBar/NavBar.js
+++ src/Components/Home/NavBar/NavBar.js
@@ -1,11 +1,102 @@
-// import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./NavBar.css";
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-md navbar-light bg-white">
+          <a className="navbar-brand" href="#test">
+            <img
+              src="https://www.payforworks.com/images/logo_dark.png"
+              alt="LOGO IMG"
+              className="logo ml-0 ml-lg-5"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-// export default class NavBar extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 this is nav bar
-//             </div>
-//         )
-//     }
-// }
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mr-5 ">
+              <li className="nav-item mt-2">
+                <a className="nav-link" href="#test">
+                  FIND JOBS
+                </a>
+              </li>
+              <li className=" dropdown dis-none mt-3">
+                <a
+                  className="dropdown-toggle "
+                  href="#test"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  FIND
+                </a>
+                <div
+                  className="dropdown-menu "
+                  aria-labelledby="navbarDropdown"
+                >
+                  <a className="dropdown-item  row mr-0" href="#test">
+                    <img
+                      src="https://www.payforworks.com/images/dd-jobs.png"
+                      alt="Payforworks-jobs-icon"
+                      className="dd-aimg mr-3"
+                    />
+                    JOBS
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item row mr-0" href="#test">
+                    <img
+                      src="https://www.payforworks.com/images/dd-fls.png"
+                      alt="Payforworks-freelancer-icon"
+                      className="dd-aimg mr-3"
+                    />
+                    FREELANCERS
+                  </a>
+                </div>
+              </li>
+              <li className="nav-item mt-2">
+                <a className="nav-link" href="#test">
+                  BROWSER FREELANCERS
+                </a>
+              </li>
+              <li className="nav-item mt-2">
+                <a className="nav-link" href="#test">
+                  HOW IT WORKS
+                </a>
+              </li>
+              <li className="nav-item"></li>
+              <li className="nav-item mt-2">
+                <a className="nav-link" href="#test">
+                  SIGN UP
+                </a>
+              </li>
+              <li className="nav-item mt-2">
+                <a className="nav-link" href="#test">
+                  LOGIN
+                </a>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-lg text-white">POST JOBS</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar;
