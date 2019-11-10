94cd6974ff16c4f20d2c176d4872c3f709bc534e
junaid91h <junaidanwar29@yahoo.com>
2019-11-10 23:27:14 +0500

    push by junaid

--- src/Components/Home/Herosection/Herosection.js
+++ src/Components/Home/Herosection/Herosection.js
@@ -3,17 +3,19 @@ import "./Herosection.css";
 export default class Herosection extends Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <div className="container-fluid mt-5">
           <div className="row">
             <div className="col-md-12 col-lg-6 hs1_conte mt-5 ">
+              <div className="hs2-dot-img"></div>
+
               <div
-                className="ml-5 mr-5 hs2_conte"
+                className="ml-5  mr-5 hs2_conte"
                 style={{ marginTop: "65px" }}
               >
                 <h2 className="head">Top quality freelancers</h2>
                 <h3 className="heading">Incredible Job results</h3>
-                <p className="mt-5 hrp" style={{ fontSize: "19px" }}>
+                <p className="mt-5 hrp">
                   Upturn your business through the top quality freelancers
                   listed here. 100's of jobs done every week on all categories
                   of web development, Wordpress development, etc
@@ -21,12 +23,14 @@ export default class Herosection extends Component {
 
                 {/* a  ke css be hy */}
 
-                <a className="btn button mb-5" href="https://www.payforworks.com">
+                <button className="btn button">
                   <span>Browse Freelancers</span>
-                </a>
+                </button>
               </div>
+              <div className="hs2-dot2-img"></div>
             </div>
-            <div className=" col-md-12 col-lg-6 ">
+            <div className=" col-md-12 col-lg-6 container-fluid hs2_px">
+              <img src="" alt="" />
               <img
                 className="img-fluid"
                 src="https://www.payforworks.com/images/hs2_px.png"
