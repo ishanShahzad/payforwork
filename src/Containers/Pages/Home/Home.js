a3dac0d98f8216450ee652a4d2004fd63f7b388c
Store- <jahangirahmedawan007@gmail.com>
2019-11-12 22:32:17 +0500

    some changes in home

--- src/Containers/Pages/Home/Home.js
+++ src/Containers/Pages/Home/Home.js
@@ -5,6 +5,7 @@ import ExpertSection from '../../../Components/Home/ExpertSection/ExpertSection'
 import Herosection from '../../../Components/Home/Herosection/Herosection';
 import GrowSection from '../../../Components/Home/GrowSection/GrowSection';
 import PeopleFavourites from '../../../Components/Home/People favourites/People_favourites';
+import HowItWorks from '../../../Components/Home/works/works';
 // import Footer from '../../../Components/Home/Footer/Footer';
 export default class Home extends Component {
   render() {
