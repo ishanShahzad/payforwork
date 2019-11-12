0e97f64392c2ecb4275a44e518a224873cbbd22c
Store- <jahangirahmedawan007@gmail.com>
2019-11-12 11:45:44 +0500

    some changes

--- src/Components/Home/ExpertSection/ExpertSection.js
+++ src/Components/Home/ExpertSection/ExpertSection.js
@@ -4,8 +4,8 @@ export default function ExpertSection() {
     return (
         <div className="background container-fluid d-flex align-items-center">
 
-            <div className="pl-3 bord pr-4 "  >
-                <h2 className="text-white head "  >
+            <div className="pl-2 bord pr-4 mt-5 "  >
+                <h2 className="text-white  head "  >
                     Find an Expert for your job</h2>
                 <p className="text-white para">from the best freelancers around the globe</p>
                 <div className="">
