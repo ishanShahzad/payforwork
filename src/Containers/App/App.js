4bea1c36bae9ea303173063332b2c1093cf5da36
junaid91h <junaidanwar29@yahoo.com>
2019-11-12 11:58:40 +0500

    push by junaid

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "../Pages/Home/Home";
-import Junaid from "../../Components/Junaid/Junaid";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Home />
-        <Junaid />
       </div>
     );
   }
