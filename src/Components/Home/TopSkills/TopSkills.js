ad6f19ecf0bd66d6f1ef935e7d58631a52d4ba16
Malik Umar <malikumar3443@gmail.com>
2019-11-13 12:03:44 +0500

    'MalikUmar'

--- src/Components/Home/TopSkills/TopSkills.js
+++ src/Components/Home/TopSkills/TopSkills.js
@@ -1,63 +1,63 @@
 import React from 'react'
-
+import "TopSkills.css" 
 export default function TopSkills() {
     return (
         <div className="container-fluid">
-            <div className="container" >
-            <h2 className="mt-5 text-dark">TopSkills</h2>
+            <div className="umar" >
+            <h2 className="mt-5 text-dark" style={{fontFamily:"nunito sans,sans-serif",fontWeight:"600",fontSize:"30px",margin:"20px 0 30px"}}>TopSkills</h2>
             <div className="row " style={{fontWeight:"lighter",fontSize:"14px"}}>
-                <div className="col-lg-3 col-md-6 col-sm-8 mt-5 mx-auto">
+                <div className="col-lg-3 cont col-md-6 col-sm-8 mx-auto">
                     <ul className="list-unstyled">
-                        <li className="a mt-2">WordPress Developer</li>
-                        <li className="mt-2">Front-End Developer</li>
-                        <li className="mt-2">JQuery</li>
-                        <li className="mt-2">UI Developer</li>
-                        <li className="mt-2">Mobile App Developer</li>
-                        <li className="mt-2">IOS Developer</li>
-                        <li className="mt-2">Logo Designer</li>
-                        <li className="mt-2">UI Designer</li>
+                        <li className="a ">WordPress Developer</li>
+                        <li className="">Front-End Developer</li>
+                        <li className="">JQuery</li>
+                        <li className="">UI Developer</li>
+                        <li className="">Mobile App Developer</li>
+                        <li className="">IOS Developer</li>
+                        <li className="">Logo Designer</li>
+                        <li className="">UI Designer</li>
                     </ul>
                 </div>
 
                 
-                <div className="col-lg-3 col-md-6 col-sm-8 mt-5 mx-auto">
+                <div className="col-lg-3 cont col-md-6 col-sm-8 mx-auto">
                     <ul className="list-unstyled">
-                        <li className="mt-2">Content Writer</li>
-                        <li className="mt-2">Codelgniter</li>
-                        <li className="mt-2">Administrative Assistant</li>
-                        <li className="mt-2">React.Js Developer</li>
-                        <li className="mt-2">Web Designer</li>
-                        <li className="mt-2">Social Media Export</li>
-                        <li className="mt-2">Marketing</li>
-                        <li className="mt-2">Ajax</li>
+                        <li className="">Content Writer</li>
+                        <li className="">Codelgniter</li>
+                        <li className="">Administrative Assistant</li>
+                        <li className="">React.Js Developer</li>
+                        <li className="">Web Designer</li>
+                        <li className="">Social Media Export</li>
+                        <li className="">Marketing</li>
+                        <li className="">Ajax</li>
                     </ul>
                 </div>
 
                 
-                <div className="col-lg-3 col-md-6 col-sm-8 mt-5 mx-auto">
+                <div className="col-lg-3 cont col-md-6 col-sm-8 mx-auto">
                     <ul className="list-unstyled">
-                        <li className="mt-2">Web Programmer</li>
-                        <li className="mt-2">UX Engineer</li>
-                        <li className="mt-2">HTML</li>
-                        <li className="mt-2">Administrator</li>
-                        <li className="mt-2">SEO Expert</li>
-                        <li className="mt-2">Laravel</li>
-                        <li className="mt-2">Responsive Designer</li>
-                        <li className="mt-2">Phonegap</li>
+                        <li className="">Web Programmer</li>
+                        <li className="">UX Engineer</li>
+                        <li className="">HTML</li>
+                        <li className="">Administrator</li>
+                        <li className="">SEO Expert</li>
+                        <li className="">Laravel</li>
+                        <li className="">Responsive Designer</li>
+                        <li className="">Phonegap</li>
                     </ul>
                 </div>
 
                 
-                <div className="col-lg-3 col-md-6 col-sm-8 mt-5 mx-auto">
+                <div className="col-lg-3 cont col-md-6 col-sm-8 mx-auto">
                     <ul className="list-unstyled">
-                        <li className="mt-2">Android Developer</li>
-                        <li className="mt-2">Node.Js</li>
-                        <li className="mt-2">Graphics Designer</li>
-                        <li className="mt-2">Legal Writing</li>
-                        <li className="mt-2">Telecommunication analist</li>
-                        <li className="mt-2">Java Developer</li>
-                        <li className="mt-2">Database Administrator</li>
-                        <li className="mt-2">Video Production</li>
+                        <li className="">Android Developer</li>
+                        <li className="">Node.Js</li>
+                        <li className="">Graphics Designer</li>
+                        <li className="">Legal Writing</li>
+                        <li className="">Telecommunication analist</li>
+                        <li className="">Java Developer</li>
+                        <li className="">Database Administrator</li>
+                        <li className="">Video Production</li>
                     </ul>
                 </div>
             </div></div>
