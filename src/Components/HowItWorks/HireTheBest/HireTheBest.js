73e03a184ae069eed53185e866ecec2865afef90
nimra <nimrahaider7860@gmail.com>
2019-11-14 12:56:56 +0500

    second-commit

--- src/Components/HowItWorks/HireTheBest/HireTheBest.js
+++ src/Components/HowItWorks/HireTheBest/HireTheBest.js
@@ -4,14 +4,15 @@ export default function HireTheBest() {
   return (
     <React.Fragment>
       <div className="container">
-        <div className="row py-5">
-          <div className="bg1 col-md-6 col-sm-6">
-            <div className="py-3"></div>
-            <img src="./1.png" alt="" />
-            <img src="./2.png" alt="" />
-            <img src="./3.png" alt="" />
+        <div className="main1 row py-5">
+          <div className="bg1 col-md-6 col-sm-12">
+            <div className="py-5">
+              <img className="img1" src="./1.png" alt="" />
+              <img className="img2" src="./2.png" alt="" />
+              <img className="img3" src="./3.png" alt="" />
+            </div>
           </div>
-          <div className="col-md-6 col-sm-6">
+          <div className="col-md-6 col-sm-12 pl-5">
             <h1 className="h1">Hire The Best freelancer</h1>
             <p className="p1 text-secondary">
               You will be receiving a number of proposals from a range of new
