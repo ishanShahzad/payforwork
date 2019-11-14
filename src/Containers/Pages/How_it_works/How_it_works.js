0fa0de6568eaec8b25607c1368c1ba989ee4ad07
Store- <jahangirahmedawan007@gmail.com>
2019-11-14 12:04:55 +0500

    new changes

--- src/Containers/Pages/How_it_works/How_it_works.js
+++ src/Containers/Pages/How_it_works/How_it_works.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import SectionOne from '../../../Components/How_it_works/Section_1/Sectioion_1'
+// import Find_experts from '../../../Components/How_it_works/Find_experts_section/Find_experts'
 
 export default class HowItWorks extends Component {
   render() {
     return (
-        <React.Fragment>
-<SectionOne/>
-        </React.Fragment>
-    
-          
-        
-     
+      <React.Fragment>
+        <SectionOne />
+        {/* <Find_experts /> */}
+      </React.Fragment>
+
+
+
+
     )
   }
 }
