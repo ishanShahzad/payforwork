2c46ca470f817e331f47aba3d4958c405e17b6a4
Aurangzaib Manzoor <aurangzaibmanzoor369@gmail.com>
2019-11-18 11:38:15 +0500

    files

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./App.css";
 import Home from "../Pages/Home/Home";
+import { Route } from "react-router-dom";
+import SignUp from "../../Components/Home/Signup/Signup";
 class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
       <React.Fragment>
-        <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/Signup" component={SignUp} />
       </React.Fragment>
-=======
-      <div className="App">
-        <Home/>
-       
-      </div>
->>>>>>> 42cf523bcd115c9fc0e1d1c60d788af07270fb25
     );
   }
 }
--- src/Containers/Pages/Home/Home.js
+++ src/Containers/Pages/Home/Home.js
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import HowItWork from "../../../Components/Home/works/works.jsx";
+import NavBar from "../../../Components/Home/NavBar/NavBar";
 const Home = () => {
   return (
     <React.Fragment>
+      <NavBar />
       <HowItWork />
     </React.Fragment>
   );
 };
 
 export default Home;
-=======
-import React, { Component } from 'react'
-import NavBar from '../../../Components/Home/NavBar/NavBar'
-import Signup from '../../../Components/Home/Signup/Signup'
-import { BrowserRouter as Router,Route} from "react-router-dom";
-
-
-export default class Home extends Component {
-    render() {
-        return (
-            <Router>
-
-                <NavBar/>
-                
-        <Route path="/Signup"exact component={Signup} />
-            </Router>
-        )
-    }
-}
->>>>>>> 42cf523bcd115c9fc0e1d1c60d788af07270fb25
